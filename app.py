969a4cac6084b9970925fa1ecaeb94b33a431bf7
balazsDombovari <dbalazs1219@gmail.com>
2019-02-24 15:33:44 +0100

    test written. readme updated. input, output file added. You can read more in "readme"

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ def hello_world():
 
 @app.route('/numbers')
 def numbers():
-    arabic_numbers = connection.get_table_from_file('arab.in')
+    arabic_numbers = connection.get_numbers_from_file('ARAB.IN')
     roman_numbers = [arabic_to_roman.get_result_under_4000(int(number)) for number in arabic_numbers]
-    connection.write_table_to_file('romai.out', roman_numbers, arabic_numbers)
+    connection.write_table_to_file('ROMAI.OUT', roman_numbers, arabic_numbers)
     return render_template('main.html', roman_numbers=roman_numbers, arabic_numbers=arabic_numbers)
 
 
