40ebc55b9c59170604a738611ec9d2c39e3a7bd8
balazsDombovari <dbalazs1219@gmail.com>
2019-02-24 11:42:09 +0100

    converting is changed from "if statements" to dictionary

--- arabic_to_roman.py
+++ arabic_to_roman.py
@@ -1,94 +1,47 @@
-def get_arabic():
-    return 3974
+def arabic_to_roman_9_dict(number):
+    roman_numbers = {
+        0: "", 1: "I", 2: "II", 3: "III", 4: "IV", 5: "V",
+        6: "VI", 7: "VII", 8: "VIII", 9: "IX"}
+    return roman_numbers[number]
 
-def arabic_to_roman_9(number):
-    if number == 0:
-        return ""
-    if number == 1:
-        return "I"
-    if number == 2:
-        return "II"
-    if number == 3:
-        return "III"
-    if number == 4:
-        return "IV"
-    if number == 5:
-        return "V"
-    if number == 6:
-        return "VI"
-    if number == 7:
-        return "VII"
-    if number == 8:
-        return "VIII"
-    if number == 9:
-        return "IX"
 
-def arabic_to_roman_90(number):
-    if number == 10:
-        return "X"
-    if number == 20:
-        return "XX"
-    if number == 30:
-        return "XXX"
-    if number == 40:
-        return "XL"
-    if number == 50:
-        return "L"
-    if number == 60:
-        return "LX"
-    if number == 70:
-        return "LXX"
-    if number == 80:
-        return "LXXX"
-    if number == 90:
-        return "XC"
 
-def arabic_to_roman_900(number):
-    if number == 100:
-        return "C"
-    if number == 200:
-        return "CC"
-    if number == 300:
-        return "CCC"
-    if number == 400:
-        return "CD"
-    if number == 500:
-        return "D"
-    if number == 600:
-        return "DC"
-    if number == 700:
-        return "DCC"
-    if number == 800:
-        return "DCCC"
-    if number == 900:
-        return "CM"
+def arabic_to_roman_90_dict(number):
+    roman_numbers = {
+        10: "X", 20: "XX", 30: "XXX", 40: "XL", 50: "L",
+        60: "LX", 70: "LXX", 80: "LXXX", 90: "XC"}
+    return roman_numbers[number]
 
-def arabic_to_roman_3000(number):
-    if number == 1000:
-        return "M"
-    if number == 2000:
-        return "MM"
-    if number == 3000:
-        return "MMM"
+
+def arabic_to_roman_900_dict(number):
+    roman_numbers = {
+        100: "C", 200: "CC", 300: "CCC", 400: "CD", 500: "D",
+        600: "DC", 700: "DCC", 800: "DCCC", 900: "CM"}
+    return roman_numbers[number]
+
+
+
+def arabic_to_roman_3000_dict(number):
+    roman_numbers = {
+        1000: "M", 2000: "MM", 3000: "MMM"}
+    return roman_numbers[number]
+
+
 
 def get_result_under_4000(number):
     arabic = ""
     if number >= 1000:
         number_1000 = number // 1000
-        arabic += arabic_to_roman_3000(number_1000 * 1000)
+        arabic += arabic_to_roman_3000_dict(number_1000 * 1000)
         number -= number_1000 * 1000
     if number >= 100:
         number_100 = number // 100
-        arabic += arabic_to_roman_900(number_100 * 100)
+        arabic += arabic_to_roman_900_dict(number_100 * 100)
         number -= number_100 * 100
     if number >= 10:
         number_10 = number // 10
-        arabic += arabic_to_roman_90(number_10 * 10)
+        arabic += arabic_to_roman_90_dict(number_10 * 10)
         number -= number_10 * 10
 
-    arabic += arabic_to_roman_9(number)
+    arabic += arabic_to_roman_9_dict(number)
     return arabic
-
-
-if __name__ == "__main__":
-    print (get_result_under_4000(get_arabic()))
